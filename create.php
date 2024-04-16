237916ca5047bc4ad3293a2eb8be9fb7d5af31cb
GUL5522 <@gmail.com>
2024-04-16 18:09:30 +0530

    this is update in database

--- create.php
+++ create.php
@@ -1,8 +1,17 @@
 <?php
     include 'header.php';
+    $db =new database();
+
+    if(isset($_POST['submit'])){
+      $name = $_POST['name'];
+      $roll = $_POST['roll'];
+      $course = $_POST['course'];
+
+      if
+    }
     ?>
     <div class="card-body">
-    <form action="/action_page.php">
+    <form action="" method="POST">
   <div class="mb-3 mt-3">
     <label for="email" class="form-label">Name:</label>
     <input type="text" class="form-control" id="name" placeholder="Enter Name" name="name">
@@ -15,10 +24,7 @@
     <label for="pwd" class="form-label">Course:</label>
     <input type="text" class="form-control" id="course" placeholder="Enter course" name="pswd">
   </div>
-  
- 
-  </div>
-  <button type="submit" class="btn btn-primary">Submit</button>
+  <button type="submit" name="submit" class="btn btn-primary">Submit</button>
   <a class="btn btn-primary" href="index.php">Go Back</a>
 </form>
     </div>
